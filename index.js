4424af837f1c75a244c258b2ba90371a345bcb77
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2022-11-24 11:39:10 +0300

    Completetd assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 1;
+let num2 = 62;
+let multiply = num1*num2
 
-// Write your code here
+
+const random = Math.ceil(Math.random());
+let num3 = 4
+let num4 = 5
+let mod = num3 % num4;
+
+let max = (1,2,19,20);
+Math.max();
